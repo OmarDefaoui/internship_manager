69f080612d93e055117005d5a4f8e2cb5177b9bf
Hayrik31 <haytambarik31@gmail.com>
2022-01-25 01:43:52 +0100

    remove id=1 in chat

--- student/chat/chat.php
+++ student/chat/chat.php
@@ -83,7 +83,7 @@ if (isset($_SESSION['id'])) {
     </div>
 
     <div id="nav_right">
-      <img src="../../assets/local_assets/images/chat.png" alt="chat" class="rounded_icon_dark" onClick="window.open('../chat/chat.php', '_self')">
+      <img src="../../assets/local_assets/images/chat.png" alt="chat" class="rounded_icon_dark" >
       <img src="../../assets/local_assets/images/notification.png" alt="notifications" class="rounded_icon_dark" onClick="window.open('../activity/activity.php', '_self')">
     </div>
   </nav>
